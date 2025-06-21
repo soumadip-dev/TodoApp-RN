72d34ad3ec2669f9d53f680bb882d187fb3af2d6
soumadip-dev <soumadipmajila@gmail.com>
2025-06-21 12:00:00 +0530

    style: create home.styles.ts and settings.styles.ts

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,43 +1,21 @@
-import { api } from '@/convex/_generated/api';
+import { createHomeStyles } from '@/assets/styles/home.styles';
 import useTheme from '@/hooks/useTheme';
-import { useMutation, useQuery } from 'convex/react';
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { LinearGradient } from 'expo-linear-gradient';
+import { StatusBar, Text } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function Index() {
-  const { toggleDarkMode } = useTheme();
+  const { toggleDarkMode, colors } = useTheme();
 
-  const todos = useQuery(api.todos.getTodos);
-  console.log('todos', todos);
-
-  const addTodo = useMutation(api.todos.addTodo);
-  const clearAllTodos = useMutation(api.todos.clearAllTodos);
+  const homeStyles = createHomeStyles(colors);
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.content}>Welcome to TODO APP🎉</Text>
-      <TouchableOpacity onPress={toggleDarkMode}>
-        <Text style={styles.content}>Toggle</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => addTodo({ text: 'Write a blog' })}>
-        <Text style={styles.content}>Add a new todo</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress={() => clearAllTodos()}>
-        <Text style={styles.content}>Clear all todos</Text>
-      </TouchableOpacity>
-    </View>
+    <LinearGradient colors={colors.gradients.background} style={homeStyles.container}>
+      <StatusBar barStyle={colors.statusBarStyle} />
+      <SafeAreaView style={homeStyles.safeArea}>
+        <Text>Welcome to TODO APP🎉</Text>
+        <Text onPress={toggleDarkMode}>Toggle Dark Mode</Text>
+      </SafeAreaView>
+    </LinearGradient>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#113F67',
-  },
-  content: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    color: '#FDF5AA',
-  },
-});
